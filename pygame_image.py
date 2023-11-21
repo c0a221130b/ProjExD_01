bbbf355a0d98a2e9856319178a49021d1a23e43a
C0A22113 <c0a221130b@edu.teu.ac.jp>
2023-11-21 15:59:07 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -19,15 +19,19 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0]) #練習4 背景画像の表示
+            
+        '''練習6'''
+        x = tmr%1600
+        #print(x)
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])#練習4 背景画像の表示
         
         '''練習5 こうかとん羽ばたく'''
         screen.blit(kk_imgs[tmr%2], [300, 200])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
