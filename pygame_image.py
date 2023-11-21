5cca3605a7ca6bef80346cde14303df1e518b0d9
C0A22113 <c0a221130b@edu.teu.ac.jp>
2023-11-21 15:40:24 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,19 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1 背景画像surfaceの生成
     tmr = 0
     
-    """練習2"""
+    '''練習2'''
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    
+    '''練習3'''
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習4 背景画像の表示
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_imgs[1], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
